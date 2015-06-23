3f814b37ec297c46b60c64b2c475d8169aa06b5d
Andrew Grant <andrewgrant243@gmail.com>
2015-06-23 15:40:04 -0400

    Initial Commit

---
+++ TennisNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TennisNotes
+//
+//  Created by Andrew Grant on 6/23/15.
+//  Copyright (c) 2015 Andrew Grant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
