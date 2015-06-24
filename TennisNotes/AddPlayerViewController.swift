acb61cb30d6176f1d6642c2752971cc041b3f336
Andrew Grant <andrewgrant243@gmail.com>
2015-06-24 10:29:53 -0400

    Completing structure of app in storyboard

---
+++ TennisNotes/AddPlayerViewController.swift
@@ -0,0 +1,105 @@
+//
+//  AddPlayerViewController.swift
+//  TennisNotes
+//
+//  Created by Andrew Grant on 6/24/15.
+//  Copyright (c) 2015 Andrew Grant. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class AddPlayerViewController: UIViewController {
+
+    @IBOutlet var opponentName: UITextField!
+    @IBOutlet var generalNotes: UITextView!
+    
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        generalNotes.layer.borderColor = UIColor.blackColor().CGColor
+        generalNotes.layer.borderWidth = 1
+        generalNotes.layer.cornerRadius = 5
+        
+        generalNotes.editable = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addOpponentPressed(sender: AnyObject) {
+        var tempName = opponentName.text
+        if isValidInput(tempName) {
+            
+            activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
+            activityIndicator.center = self.view.center
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+            view.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+            
+            var opponents = PFObject(className:"Opponents")
+            self.initializeSections(opponents, name: tempName)
+            
+            opponents.saveInBackgroundWithBlock {
+                (success: Bool, error: NSError?) -> Void in
+                self.activityIndicator.stopAnimating()
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                if (success) {
+                    // The object has been saved.
+                    self.clearAllFields()
+                } else {
+                    // There was a problem, check error.description
+                     self.clearAllFields()
+                }
+            }
+        }
+    }
+
+    func initializeSections(object: PFObject, name: String){
+        object["Follower"] = PFUser.currentUser()!.username
+        object["OpponentFollowed"] = name
+        object["General"] = generalNotes.text
+        object["Groundstrokes"] = ""
+        object["Serve"] = ""
+        object["Volleys"] = ""
+        object["Mental"] = ""
+        object["Physical"] = ""
+        object["Tactics"] = ""
+        
+        object["TypeFollower"] = userType
+        
+    }
+    
+    func isValidInput(oppInput: String) -> Bool{
+        
+        if oppInput != "" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func clearAllFields(){
+        self.opponentName.text = ""
+        generalNotes.text = ""
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TennisNotes/OpponentsTableViewController.swift
+++ TennisNotes/OpponentsTableViewController.swift
@@ -9,8 +9,14 @@
 import UIKit
 import Parse
 
+var opponentsList = [String]()
+var opponentsID = [String]()
+var rowSelected = 0
+
 class OpponentsTableViewController: UITableViewController {
 
+    @IBOutlet var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +25,41 @@ class OpponentsTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        if userType == "coach" {
+            self.titleLabel.text = "Students"
+        } else {
+            self.titleLabel.text = "Opponents"
+        }
+        
+        var query = PFQuery(className:"Opponents")
+        query.whereKey("Follower", equalTo: PFUser.currentUser()!.username!)
+        query.whereKey("TypeFollower", equalTo: userType)
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]?, error: NSError?) -> Void in
+            
+            opponentsList.removeAll(keepCapacity: true)
+            opponentsID.removeAll(keepCapacity: true)
+            
+            if error == nil {
+                // The find succeeded.
+                println("Successfully retrieved \(objects!.count) scores.")
+                // Do something with the found objects
+                if let objects = objects as? [PFObject] {
+                    for object in objects {
+                        println(object.objectId)
+                        var tempID = object.objectId
+                        opponentsID.append(tempID!)
+                        var tempString = object["OpponentFollowed"] as! String
+                        opponentsList.append(tempString)
+                    }
+                }
+            } else {
+                // Log details of the failure
+                println("Error: \(error!) \(error!.userInfo!)")
+            }
+            self.tableView.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,25 +72,92 @@ class OpponentsTableViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return opponentsList.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
 
         // Configure the cell...
 
+       cell.textLabel?.text = opponentsList[indexPath.row]
+        
         return cell
     }
-    */
+    
 
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        rowSelected = indexPath.row
+        self.performSegueWithIdentifier("jumpToOppShotList", sender: self)
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            var tempId = opponentsID[indexPath.row]
+            
+            var query = PFQuery(className:"Opponents")
+            
+            query.getObjectInBackgroundWithId(tempId) {
+                (opp: PFObject?, error: NSError?) -> Void in
+                if error == nil && opp != nil {
+                    println(opp)
+                    var object: AnyObject = opp!
+                    object.deleteInBackgroundWithTarget(nil, selector: nil)
+                } else {
+                    println(error)
+                }
+            }
+        
+            opponentsList.removeAtIndex(indexPath.row)
+            opponentsID.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        var query = PFQuery(className:"Opponents")
+        query.whereKey("Follower", equalTo: PFUser.currentUser()!.username!)
+        query.whereKey("TypeFollower", equalTo: userType)
+        
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]?, error: NSError?) -> Void in
+            
+            
+            opponentsList.removeAll(keepCapacity: true)
+            opponentsID.removeAll(keepCapacity: true)
+            
+            if error == nil {
+                // The find succeeded.
+                println("Successfully retrieved \(objects!.count) scores.")
+                // Do something with the found objects
+                if let objects = objects as? [PFObject] {
+                    for object in objects {
+                        println(object.objectId)
+                        var tempID = object.objectId
+                        opponentsID.append(tempID!)
+                        var tempString = object["OpponentFollowed"] as! String
+                        opponentsList.append(tempString)
+                    }
+                }
+            } else {
+                // Log details of the failure
+                println("Error: \(error!) \(error!.userInfo!)")
+            }
+            self.tableView.reloadData()
+        }
+
+        
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
